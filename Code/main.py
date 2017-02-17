47c120bc6dcd10fbcd2bc78e0741ebe9dda6736b
WJSWebster <zbac013@live.rhul.ac.uk>
2017-02-17 22:32:06 +0000

    removed unused method

--- Code/main.py
+++ Code/main.py
@@ -510,18 +510,6 @@ def getFlagCoords(mapName = "Main"):  # again, only if input argument is blank (
 		flagCoords.append(((((mx - 1) * 80 / 2.2) + map_Coords[0]), (((my - 1) * 80 / 2.2) + map_Coords[1])))  # note: this may be problematic if x or y = 1?
 	return flagCoords
 
-def between(value, a, b):
-    # Find and validate before-part.
-    pos_a = value.find(a)
-    if pos_a == -1: return ""
-    # Find and validate after part.
-    pos_b = value.rfind(b)
-    if pos_b == -1: return ""
-    # Return middle part.
-    adjusted_pos_a = pos_a + len(a)
-    if adjusted_pos_a >= pos_b: return ""
-    return value[adjusted_pos_a:pos_b]
-
 def generateMap (flagCoords):
 	global grid_List
 	print "this method is to generate the grid_List when play begins"
